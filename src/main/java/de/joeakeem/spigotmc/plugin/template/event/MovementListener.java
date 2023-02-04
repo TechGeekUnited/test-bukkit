10c37bb0807f8a16772e92993399c473076c54aa
Tech Geek United <79381496+TechGeekUnited@users.noreply.github.com>
2023-02-03 20:44:22 -0500

    Update MovementListener.java

--- src/main/java/de/joeakeem/spigotmc/plugin/template/event/MovementListener.java
+++ src/main/java/de/joeakeem/spigotmc/plugin/template/event/MovementListener.java
@@ -1,40 +1,17 @@
-package de.joeakeem.spigotmc.plugin.template.event;
-
-import de.joeakeem.spigotmc.plugin.template.TemplatePlugin;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleDebugEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
-
-public class MovementListener implements Listener {
-
-    private final Logger logger;
-
-    public MovementListener(TemplatePlugin plugin) {
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
-        this.logger = plugin.getLogger();
+public class DisablePieray extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
     }
-
+    
     @EventHandler
-    public void movement(PlayerMoveEvent event) {
-        Location loc = event.getPlayer().getLocation();
-        loc.setY(loc.getY() - 1);
-        Block b = loc.getBlock();
-        Material material = b.getType();
-        switch (material) {
-            case WATER:
-                b.setType(Material.ICE);
-                break;
-            case GRASS:
-                b.setType(Material.ACACIA_PLANKS);
-                break;
-            case SAND:
-                b.setType(Material.BEDROCK);
-                break;
-        }
+    public void onPlayerToggleDebug(PlayerToggleDebugEvent event) {
+        event.setCancelled(true);
+        event.getPlayer().sendMessage("Pieray is disabled in this server.");
     }
 }
